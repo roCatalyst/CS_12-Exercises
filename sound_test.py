c596d171516f62f888ad7dd98d3b554075b660af
Lucas Miguel V. Agcaoili <lvagcaoili@up.edu.ph>
2024-05-03 14:11:47 +0800

    changed music files

--- sound_test.py
+++ sound_test.py
@@ -24,72 +24,10 @@ class Game:
     def __init__(self):
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="sound test", fps=30)
         pyxel.mouse(visible=True)
-        self.init_music()
+        pyxel.load("PYXEL_RESOURCE_FILE.pyxres")
         self.play_button = Button(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, 10, 10)
         pyxel.run(self.update, self.draw)
-
-    def init_music(self):
-        pyxel.sounds[0].set(
-            "d2d2d3ra2rrg#2 rg2rf2rd2f2g2 c2c2d3ra2rrg#2 rg2rf2rd2f2g2",
-            "s",
-            "4",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.sounds[1].set(
-            "b1b1d3ra2rrg#2 rg2rf2rd2f2g2 a#1a#1d3ra2rrg#2 rg2rf2rd2f2g2",
-            "s",
-            "4",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.sounds[2].set(
-            "a1ra1ra1a1ra1 ra1ra1a1ra1r g1rg1rg1g1rg1 rg1rg1g1rg1r",
-            "s",
-            "2",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.sounds[3].set(
-            "d1rd1rd1d1rd1 rd1rd1d1rd1r c1rc1rc1c1rc1 rc1rc1c1rc1r",
-            "s",
-            "2",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.sounds[4].set(
-            "g1rg1rg1g1rg1 rg1rg1g1rg1r f1rf1rf1f1rg1 rg1rg1g1rg1r",
-            "s",
-            "2",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.sounds[5].set(
-            "b0rb0rb0b0rb0 rb0rb0b0rb0r a#0ra#0ra#0a#0rc1 rc1rc1c1rc1r",
-            "s",
-            "2",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.sounds[63].set(
-            "rrrrrrrr rrrrrrrr rrrrrrrr rrrrrrrr",
-            "s",
-            "2",
-            "vvvvvvvv",
-            20
-        )
-
-        pyxel.musics[0].set(
-            [0, 1, 0, 1],
-            [63, 63, 2, 4],
-            [63, 63, 3, 5]
-        )
+      
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
